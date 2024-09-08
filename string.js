ce6498c07203cbc9ccfc6df44b765cf027e593c3
Mayur <gmayur49k@gmail.com>
2024-09-08 19:43:31 +0530

    String and its method added

---
+++ string.js
@@ -0,0 +1,20 @@
+// const name = "Mayur"
+// console.log(name);
+// let newValue = new String("Alphabet");
+// console.log(newValue.charAt(2));
+// console.log(newValue.length);
+// console.log(newValue.includes('ph'))
+// console.log(newValue.toUpperCase());
+// console.log(newValue.substring(0,4));
+// console.log(newValue.slice(0,5))
+// console.log(newValue)
+// const newString = new String("      TheNameIsBrand")
+// console.log(newString)
+// console.log(newString.trim())
+// Trim is used to remove whitespaces from start and end of the string 
+// Trim start and trim end are also available
+
+let gamewin = new String("The King is the Owner of the World")
+console.log(gamewin.split(' '));
+// Split method splits the string on the basis of pattern provided it then searches for the pattern in the string  and divides the string into ordered list of substrings puts this substring returns an array based on that matching pattern
+
