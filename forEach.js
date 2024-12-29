ac277a3095c003a36d0f8905c73acc9d3765116d
Mayur <mayur.gaikwad@avaali.com>
2024-12-29 18:58:37 +0530

    [array methods , and html]

---
+++ forEach.js
@@ -0,0 +1,7 @@
+// forEach Loop in array 
+let fruits = ["Mango","Watermelon","grapes","pineapple","guava"];
+let altFruit = []; 
+fruits.forEach((fruit,index)=>{
+   altFruit.push(fruit);
+})
+console.log(altFruit); // altFruit array populated with the values from our array
